6755385dfd1af7ebbb825de5339cc37c0f85f942
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2022-07-08 21:22:24 +0300

    Добавила программу номер 1

--- project_1/Program.cs
+++ project_1/Program.cs
@@ -2,3 +2,11 @@
 // принимает число и выдаёт его квадрат (число
 // умноженное на само себя)
 
+Console.Clear();
+string str = "Привет!";
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int num = 4;
+int res = num * num;
+Console.WriteLine($"Квадрат числа {num} = {res}");
